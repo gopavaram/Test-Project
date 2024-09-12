f1a31e6035d4e1e6ea772aada3017f50e2a6c486
Varun Reddy Gopavaram <varun.gopavaram@gmail.com>
2024-09-12 08:36:46 -0400

    updae

--- PythonApplication1/PythonApplication1.py
+++ PythonApplication1/PythonApplication1.py
@@ -2,3 +2,4 @@
 print("My First Python program on Visual studios")
 print("I am making a change to the existing code to check git options and to commit new code")
 print("Making a 3rd change")
+print("cloned it and pushing a new update")
