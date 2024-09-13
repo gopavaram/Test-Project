f7bf784154a28c0831bec9b584a3c6dfed131f89
Varun Reddy Gopavaram <varun.gopavaram@gmail.com>
2024-09-12 20:10:08 -0400

    commit3

--- PythonApplication1/PythonApplication1.py
+++ PythonApplication1/PythonApplication1.py
@@ -2,4 +2,3 @@
 print("My First Python program on Visual studios")
 print("I am making a change to the existing code to check git options and to commit new code")
 print("Making a 3rd change")
-print("cloned it and pushing a new update")
